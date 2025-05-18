3704d993458061a19c9f38140da39938713ecf9d
leverimmy <leverimmy@gmail.com>
2025-05-18 17:50:18 +0800

    chore: remove minOS

--- minOS/modules/axhal/src/platform/aarch64_common/mod.rs
+++
@@ -1,8 +0,0 @@
-mod boot;
-
-pub mod generic_timer;
-#[cfg(not(platform_family = "aarch64-raspi"))]
-pub mod psci;
-
-#[cfg(not(platform_family = "aarch64-bsta1000b"))]
-pub mod pl011;
--- minOS/test_minOS.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm pflash.img
-rm disk.img
-
-make pflash_img
-make disk_img
-
-make run A=app/hello_world
